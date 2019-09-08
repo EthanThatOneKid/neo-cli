bef1c072ec16183269d67bba6c00b6b4acd1ba58
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2019-09-08 02:46:44 -0700

    setting up files for development

---
+++ index.js
@@ -0,0 +1,8 @@
+// Dependencies
+const puppeteer = require('puppeteer');
+const Neo = require('./lib/Neo');
+
+// Main Process
+const entryFilePath = process.argv[1];
+const neo = Neo.load(entryFilePath);
+console.log({neo});
