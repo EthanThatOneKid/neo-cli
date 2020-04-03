f1b6c320144c355519a199cb8b81625a74150831
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-04-03 15:42:22 -0700

    Create keywords.js

---
+++ src/keywords.js
@@ -0,0 +1,18 @@
+export default keywords = {
+  AWAIT: "await",
+  CLICK: "click",
+  DIALOG: "dialog",
+  FIELD: "field",
+  GOTO: "goto",
+  LOG: "log",
+  NEO: "neo",
+  PAUSE: "pause",
+  READ: "read",
+  REP: "rep",
+  SAV: "sav",
+  SEL: "sel",
+  SHOOT: "shoot",
+  TITLE: "title",
+  UNTIL: "until",
+  VAR: "var"
+};
